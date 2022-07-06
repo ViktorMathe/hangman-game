650e6c69aecfa71f4512aea8b87a2889b2a9227c
ViktorMathe <matheviktor@icloud.com>
2022-07-06 23:32:57 +0000

    Add a menu to the terminal to choose between two options

--- run.py
+++ run.py
@@ -1,3 +1,56 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def startgame():
+    print("Game will start")
+
+
+def instuctions():
+    print("This will tell you what to do")
+
+
+startgame()
+
+
+instuctions()
+
+menu = {}
+menu['1'] = 'Start game'
+menu['2'] = 'Instructions'
+
+
+def menu():
+
+    """
+    Menu to start a game or read the instructions to how to play!
+    """
+
+    print("""
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+                   """)
+
+    print()
+    choice = input("""
+                    1.Start Game
+                    2.Instructions
+                 """)
+    if choice == '1':
+        startgame()
+    elif choice == '2':
+        instuctions()
+    elif choice == '':
+        None
+        
+    else:
+        print('Invalid choice,please choose from option 1 or 2!')
+        menu()
+    
+
+menu()
