c8b69c91534c09af4693a5a858644139e50917a2
ViktorMathe <matheviktor@icloud.com>
2022-07-07 13:20:53 +0000

    Add a clear method which is clears the terminal when the game starts

--- run.py
+++ run.py
@@ -1,32 +1,26 @@
 import random
+import sys
+from os import system, name
 
 
-def startgame():
-    print("Game will start")
-
-
-def instuctions():
-    print("This will tell you what to do")
-
-
-startgame()
-
-
-instuctions()
-
-menu = {}
-menu['1'] = 'Start game'
-menu['2'] = 'Instructions'
-
-
-def menu():
-
+def clear():
     """
-    Menu to start a game or read the instructions to how to play!
+    Clear the terminal if Windows it is using
+    the 'cls' statement otherwise (Linux or Mac)
+    it uses the 'clear' statement
     """
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
+
+def logo():
+    """
+    Hangman logo
+    """
     print("""
- _                                             
+ _                                            
 | |                                            
 | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
 | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
@@ -36,21 +30,66 @@ def menu():
                    |___/                       
                    """)
 
-    print()
+
+hangmans = ["  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+
+        "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+
+        "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+
+        "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+
+        "  +---+\n  |   |\n  O   |\n /|\  |\n      |\n      |\n=========",
+
+        "  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========",
+
+        "  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========="]
+
+
+def startgame():
+    """
+    This method will start the game from scratch
+    """
+    clear()
+    logo()
+    print(hangmans[6])
+
+
+def instuctions():
+    """
+    This will tell you how to play the game
+    """
+    print("This will tell you what to do")
+
+
+menu = {}
+menu['1'] = 'Start game'
+menu['2'] = 'Instructions'
+menu['3'] = 'Exit game'
+
+
+def main_menu():
+
+    """
+    Menu to start a game or read the instructions to how to play!
+    """
+    logo()
     choice = input("""
                     1.Start Game
                     2.Instructions
+                    3.Exit game
                  """)
     if choice == '1':
         startgame()
     elif choice == '2':
         instuctions()
+    elif choice == '3':
+        sys.exit()
     elif choice == '':
         None
-        
     else:
         print('Invalid choice,please choose from option 1 or 2!')
-        menu()
-    
+        main_menu()
 
-menu()
+
+main_menu()
